8b793a28a216a709eb4a5775f51b786b8d250d3f
Ricardo Costa da Silva <rcsilva2405@gmail.com>
2023-05-09 16:26:18 -0500

    Arquivos css e js criados

---
+++ js/script.js
@@ -0,0 +1,9 @@
+async function fetchPokemon(pokemon) {
+  // Como basicamente se pega os dados da api
+  const APIresponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
+  const data = await APIresponse.json();
+
+  console.log(data.id);
+}
+
+fetchPokemon("pichu");
