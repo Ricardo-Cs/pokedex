042fcd2636c5cc99aa04fea55deb827f2d169dab
Ricardo Costa da Silva <rcsilva2405@gmail.com>
2023-05-10 15:45:07 -0500

    Projeto Finalizado

--- js/script.js
+++ js/script.js
@@ -1,9 +1,60 @@
-async function fetchPokemon(pokemon) {
-  // Como basicamente se pega os dados da api
-  const APIresponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
-  const data = await APIresponse.json();
+const pokemonName = document.querySelector(".pokemon-name");
+const pokemonId = document.querySelector(".pokemon-number");
+const pokemonImage = document.querySelector(".pokemon-image");
 
-  console.log(data.id);
+const form = document.querySelector(".form");
+const search = document.querySelector(".search");
+const prevButton = document.querySelector(".prev-button");
+const nextButton = document.querySelector(".next-button");
+
+
+let searchPokemon = 1;
+
+// Pega os dados na API de acordo com o pokemon passado como parâmetro
+async function fetchPokemon(pokemon) {
+  const APIresponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
+
+  if (APIresponse.status === 200) {
+    const data = await APIresponse.json();
+    return data;
+  }
 }
 
-// fetchPokemon("pichu");
+// Insere os dados retornados no HTML
+async function insertData(pokemon) {
+  pokemonName.innerHTML = "Loading...";
+  pokemonId.innerHTML = '';
+  const data = await fetchPokemon(pokemon);
+
+  if(data) {
+    pokemonImage.style.display = 'block';
+    pokemonImage.src = data['sprites']['front_default'];
+    pokemonId.innerHTML = data.id;
+    pokemonName.innerHTML = data.name;
+
+    searchPokemon = data.id;
+    search.value = '';
+  } else {
+    pokemonImage.style.display = 'none';
+    pokemonName.innerHTML = "Not Found";
+    pokemonId.innerHTML = "";
+  }
+}
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  insertData(search.value.toLowerCase());
+});
+
+prevButton.addEventListener("click", () => {
+  if (searchPokemon > 1) {
+    searchPokemon -= 1;
+    insertData(searchPokemon);
+  }
+});
+
+nextButton.addEventListener("click", () => {
+  searchPokemon += 1;
+  insertData(searchPokemon);
+});
+
+insertData(searchPokemon);
